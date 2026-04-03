d3fb5c28dd04bd07f111b2a9e58125ae7c9809d5
Юрий Лешок <urijlesok@MacBook-Air-Urij.local>
2026-04-03 16:32:45 +0300

    Remove debug endpoint

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ const API_KEY = process.env.LIVEKIT_API_KEY;
 const API_SECRET = process.env.LIVEKIT_API_SECRET;
 
 app.get('/', (req, res) => {
-  res.json({
-    status: 'ok',
-    hasApiKey: !!process.env.LIVEKIT_API_KEY,
-    hasApiSecret: !!process.env.LIVEKIT_API_SECRET,
-  });
+  res.json({ status: 'ok' });
 });
 
 app.post('/token', async (req, res) => {
